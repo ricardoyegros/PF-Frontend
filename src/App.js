c99b2fe1dad73c7aad94865d1ed78aef7d8558a8
Ricardo Yegros <ricardo_yegros@hotmail.com>
2022-09-26 20:12:15 -0300

    feat/creacion carpetas y componente footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
     <div className="App">
- 
+      <Footer />
     </div>
   );
 }
