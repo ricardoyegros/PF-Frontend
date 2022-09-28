34c1259289e599be0e827152537780420cd07cf4
Mauricioam <mauricioam91@gmail.com>
2022-09-28 18:56:55 -0300

    feat/searchInput: falta linkear datos con back y el reducer para estado de home

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,7 @@
 
 export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
-export const DETAIL_PRODUCT = "DETAIL_PRODUCT"
+export const DETAIL_PRODUCT = "DETAIL_PRODUCT";
+export const SEARCH_PRODUCT = "SEARCH_PRODUCT"
 
 export function getAllProducts () {
     return async function (dispatch){
@@ -16,8 +17,19 @@ export function getDetailProduct(i){
         let detailProduct =  await fetch(`https://rickandmortyapi.com/api/character/${i}`).then(
             res => res.json()
         ).then(product => product)
-        console.log(detailProduct,"actions")
+     
         return dispatch({type:DETAIL_PRODUCT,payload:detailProduct})
 
+    }
+}
+//aqui funcion para el fecth de la ruta de busqueda
+export function getItem(product){
+    return async function(dispatch){
+        let item = await fetch(`https://rickandmortyapi.com/api/character/?name=${product}`).then(
+            res => res.json()
+        ).then(item => item)
+        .catch(error => alert(error))
+        return dispatch({type:SEARCH_PRODUCT,payload: item})
+        
     }
 }
