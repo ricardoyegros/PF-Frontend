b573d2f44d1242e3a537c598636cd2339f29f20f
IgnacioOrellana <ignacio97orellana@gmail.com>
2022-09-29 20:51:48 -0300

    no mergear

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import {React, useEffect, useState} from "react";
 import Navbar from "./NavBar";
 import Footer from "./Footer";
 import {useSelector, useDispatch} from "react-redux";
-import { getAllProducts } from "../redux/actions/index.js";
+import { getAllProducts } from "../redux/actions/actionAllProducts.js";
 import Card from "./Card";
 import CategoryFilter from "./CategoryFilter.jsx";
 import Pagination from "@mui/material/Pagination";
@@ -10,7 +10,7 @@ import './Styles/cards.css'
 
 
 export default function Home () {
-    const allProducts = useSelector(state => state.allProducts);
+    const allProducts = useSelector(state => state.allProductsReducer.allProducts);
     const dispatch = useDispatch();
     const [currentPage, setCurrentPage] = useState(1)
     const cardsPerPage = 5
@@ -22,13 +22,12 @@ export default function Home () {
         dispatch(getAllProducts());
     }, [dispatch]);
 
-    // let products = allProducts.slice(0,5);
-
     const handleOnChange = (e, page) => {
         console.log(e, page)
         e.preventDefault();
         setCurrentPage(page)
     }
+    
     return (
         <div>
             <Navbar/>
