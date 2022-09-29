9cbad8aa22118317244784139bf55eca08a1f0b5
Ricardo Yegros <ricardo_yegros@hotmail.com>
2022-09-29 20:40:11 -0300

    formulario de creacion de productos

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,35 +1,47 @@
-
+import axios from "axios";
 export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
 export const DETAIL_PRODUCT = "DETAIL_PRODUCT";
-export const SEARCH_PRODUCT = "SEARCH_PRODUCT"
+export const SEARCH_PRODUCT = "SEARCH_PRODUCT";
 
-export function getAllProducts () {
-    return async function (dispatch){
-        let allProducts = await fetch("https://my-json-server.typicode.com/ricardoyegros/mockForTesting/db").then(
-            res => res.json()
-        ).then(products => products.results);
-        return dispatch({type: GET_ALL_PRODUCTS, payload: allProducts});
-    }
-};
+export function getAllProducts() {
+  return async function (dispatch) {
+    let allProducts = await fetch(
+      "https://pf-tech-store.herokuapp.com/products"
+    )
+      .then((res) => res.json())
+      .then((products) => products);
+    return dispatch({ type: GET_ALL_PRODUCTS, payload: allProducts });
+  };
+}
 
-export function getDetailProduct(i){
-    return async function(dispatch){
-        let detailProduct =  await fetch(`https://rickandmortyapi.com/api/character/${i}`).then(
-            res => res.json()
-        ).then(product => product)
-     
-        return dispatch({type:DETAIL_PRODUCT,payload:detailProduct})
+export function getDetailProduct(i) {
+  return async function (dispatch) {
+    let detailProduct = await fetch(
+      `https://rickandmortyapi.com/api/character/${i}`
+    )
+      .then((res) => res.json())
+      .then((product) => product);
 
-    }
+    return dispatch({ type: DETAIL_PRODUCT, payload: detailProduct });
+  };
 }
 //aqui funcion para el fecth de la ruta de busqueda
-export function getItem(product){
-    return async function(dispatch){
-        let item = await fetch(`https://rickandmortyapi.com/api/character/?name=${product}`).then(
-            res => res.json()
-        ).then(item => item)
-        .catch(error => alert(error))
-        return dispatch({type:SEARCH_PRODUCT,payload: item})
-        
-    }
+export function getItem(product) {
+  return async function (dispatch) {
+    let item = await fetch(
+      `https://rickandmortyapi.com/api/character/?name=${product}`
+    )
+      .then((res) => res.json())
+      .then((item) => item)
+      .catch((error) => alert(error));
+    return dispatch({ type: SEARCH_PRODUCT, payload: item });
+  };
+}
+
+export function createProduct(form) {
+  return function (dispatch) {
+    axios
+      .post("https://pf-tech-store.herokuapp.com/products", form)
+      .catch((error) => console.log(error));
+  };
 }
