e6137e7b5ca6426fac3eb297f25baf01a97f35d1
Julian David Nuñez <juliandavidnunesfranco@hotmail.es>
2022-10-01 00:08:15 -0500

    feat(components CreateUsers): se ultima el formulario register

--- src/components/CreateUsers.jsx
+++ src/components/CreateUsers.jsx
@@ -2,19 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { createUsers } from "../redux/actions/index.js";
-
-import OutlinedInput from "@mui/material/OutlinedInput";
 import MenuItem from "@mui/material/MenuItem";
-import ListItemText from "@mui/material/ListItemText";
-import Select from "@mui/material/Select";
-//import Checkbox from "@mui/material/Checkbox";
-import InputLabel from "@mui/material/InputLabel";
-import FormControl from "@mui/material/FormControl";
-
-//import clear from "../redux/actions/index";
-
-//import { useHistory } from "react-router-dom";
-
 import {
     Typography,
     Card,
@@ -24,68 +12,33 @@ import {
     Button,
 } from "@mui/material";
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-    PaperProps: {
-        style: {
-            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-            width: 250,
-        },
-    },
-};
+import { useNavigate } from "react-router-dom";
 
 export default function CreateUsers() {
     const dispatch = useDispatch();
-    //const [state, setState] = useState({});
-    const [typeIdent, setTypeIdent] = useState([]);
-    const namesDNI = ["CC", "DNI", "PASSPORT", "GREENCARD", "OTRO"];
+    const navigate = useNavigate();
 
-    const [input, setInput] = useState({
-        name: "",
-        lastName: "",
-        typeIdentification: "",
-        identification: "",
-        contact: "",
-        email: "",
-        address: "",
-        password: "",
-    });
+    const namesDNI = [
+        { value: "CC", label: "CC" },
+        { value: "DNI", label: "DNI" },
+        { value: "PASSPORT", label: "PASSPORT" },
+        { value: "GREENCARD", label: "GREENCARD" },
+        { value: "OTRO", label: "OTRO" },
+    ];
 
-    //==========el change del select
-    const handleChangeTypeIdent = (event) => {
-        const {
-            target: { value },
-        } = event;
-        setTypeIdent(
-            // On autofill we get a stringified value.
-            typeof value === "string" ? value.split(",") : value
-        );
-    };
-    console.log(typeIdent);
+    const [input, setInput] = useState({});
 
-    //======>>>>>>> el change de los input's
+    console.log(input);
+
     function handleChange(e) {
         setInput({ ...input, [e.target.name]: e.target.value });
     }
 
-
-    //======>>>>>>> el change del boton submit
     function handleSubmit(e) {
         e.preventDefault();
-
         dispatch(createUsers(input));
-        setInput({
-            name: "",
-            lastName: "",
-            typeIdentification: "",
-            identification: "",
-            contact: "",
-            email: "",
-            address: "",
-            password: "",
-        });
-        // dispatch(clear(dispatch));
+        setInput({});
+        navigate("/");
     }
 
     return (
@@ -100,11 +53,7 @@ export default function CreateUsers() {
                     <Typography gutterBottom variant="h5">
                         Create a New User!
                     </Typography>
-                    <form
-                        onSubmit={(e) => {
-                            handleSubmit(e);
-                        }}
-                    >
+                    <form onSubmit={handleSubmit}>
                         <Grid container spacing={1}>
                             <Grid xs={12} item>
                                 <TextField
@@ -131,40 +80,36 @@ export default function CreateUsers() {
                                 />
                             </Grid>
                             <Grid xs={12} item>
-                                <FormControl sx={{ m: 1, width: 300 }}>
-                                    <InputLabel id="demo-multiple-checkbox-label">
-                                        Type Identification
-                                    </InputLabel>
-                                    <Select
-                                        labelId="demo-multiple-checkbox-label"
-                                        id="demo-multiple-checkbox"
-                                        value={typeIdent}
-                                        onChange={handleChangeTypeIdent}
-                                        input={
-                                            <OutlinedInput label="TypeIdent" />
-                                        }
-                                        renderValue={(selected) =>
-                                            selected.join(", ")
-                                        }
-                                        MenuProps={MenuProps}
-                                    >
-                                        {namesDNI.map((name) => (
-                                            <MenuItem key={name} value={name}>
-                                                <ListItemText primary={name} />
-                                            </MenuItem>
-                                        ))}
-                                    </Select>
-                                </FormControl>
+                                <TextField
+                                    select
+                                    label="Type Identification"
+                                    placeholder="Please enter a type identification..."
+                                    variant="outlined"
+                                    fullWidth
+                                    required
+                                    name="typeIdentification"
+                                    value={input.typeIdentification}
+                                    onChange={handleChange}
+                                >
+                                    {namesDNI.map((option) => (
+                                        <MenuItem
+                                            key={option.value}
+                                            value={option.value}
+                                        >
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
                             </Grid>
                             <Grid xs={12} item>
                                 <TextField
-                                    label="Identification"
+                                    label="identification"
                                     placeholder="Please enter you number of identification..."
                                     variant="outlined"
                                     type="number"
                                     fullWidth
                                     required
-                                    name="Identification"
+                                    name="identification"
                                     value={input.identification}
                                     onChange={handleChange}
                                 />
@@ -215,7 +160,8 @@ export default function CreateUsers() {
                                     variant="outlined"
                                     fullWidth
                                     required
-                                    name="address"
+                                    type="password"
+                                    name="password"
                                     value={input.password}
                                     onChange={handleChange}
                                 />
