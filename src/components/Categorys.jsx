4874a5e8f991e0554ad3f8cf2f24e1a80d2f7350
Miqueas Ledesma <Mickeas98@gmail.com>
2022-10-03 21:32:44 -0300

    fix-paginated/filters/final

--- src/components/Categorys.jsx
+++ src/components/Categorys.jsx
@@ -10,11 +10,10 @@ import {
 import { useState } from "react";
 import { Box } from "@mui/system";
 import CardProduct2 from "./Card2";
-import { findAllByDisplayValue } from "@testing-library/react";
 
 export default function Categorys() {
   const dispatch = useDispatch();
-  const [state, setState] = useState({});
+  const [state, setState] = useState({ sort: "DESC", type: "id" });
   const [page, setPage] = useState(1);
   useEffect(() => {
     dispatch(getCategoryNames());
@@ -34,68 +33,217 @@ export default function Categorys() {
 
   let pages = reduxState2 || 1;
 
-  // console.log(reduxState4.search);
-  // console.log(reduxState4.name);
-
   const handleReset = () => {
-    setState({});
-    dispatch(preFilter({}));
-    dispatch(isInUse({}));
+    setPage(1);
+    setState({ sort: "DESC", type: "id", page: "" });
+    dispatch(preFilter({ sort: "DESC", type: "id", page: "" }));
+    dispatch(isInUse({ sort: "DESC", type: "id", page: "" }));
   };
 
   const handleCategory = (e) => {
     setPage(1);
     if (state.categoryName === e.target.name) {
-      console.log(reduxState4)
-      setState({ ...state, categoryId: '', categoryName: false, page: '', name: reduxState4.name });
-      dispatch(preFilter({ ...state, categoryId: '', categoryName: false, page: '', name: reduxState4.name }));
-      dispatch(isInUse({ ...state, categoryId: '', categoryName: false }));
-
+      setState({
+        ...state,
+        categoryId: "",
+        categoryName: false,
+        page: "",
+        name: reduxState4.name,
+      });
+      dispatch(
+        preFilter({
+          ...state,
+          categoryId: "",
+          categoryName: false,
+          page: "",
+          name: reduxState4.name,
+        })
+      );
+      dispatch(isInUse({ ...state, categoryId: "", categoryName: false }));
     } else {
-      console.log(reduxState4)
-      setState({ ...state, categoryId: e.target.value, categoryName: e.target.name, page: '', name: reduxState4.name });
-      dispatch(preFilter({ ...state, categoryId: e.target.value, categoryName: e.target.name, page: '', name: reduxState4.name }));
-      dispatch(isInUse({ ...state, categoryId: e.target.value, categoryName: e.target.name, name: reduxState4.name }));
+      setState({
+        ...state,
+        categoryId: e.target.value,
+        categoryName: e.target.name,
+        page: "",
+        name: reduxState4.name,
+      });
+      dispatch(
+        preFilter({
+          ...state,
+          categoryId: e.target.value,
+          categoryName: e.target.name,
+          page: "",
+          name: reduxState4.name,
+        })
+      );
+      dispatch(
+        isInUse({
+          ...state,
+          categoryId: e.target.value,
+          categoryName: e.target.name,
+          name: reduxState4.name,
+        })
+      );
     }
   };
 
   const handleBrand = (e) => {
     setPage(1);
     if (state.brandName === e.target.name) {
-      console.log(reduxState4)
-      setState({ ...state, brandId: '', brandName: false, page: '', name: reduxState4.name });
-      dispatch(preFilter({ ...state, brandId: '', brandName: false, page: '', name: reduxState4.name }));
-      dispatch(isInUse({ ...state, brandId: '', brandName: false, name: reduxState4.name }))
+      console.log(reduxState4);
+      setState({
+        ...state,
+        brandId: "",
+        brandName: false,
+        page: "",
+        name: reduxState4.name,
+      });
+      dispatch(
+        preFilter({
+          ...state,
+          brandId: "",
+          brandName: false,
+          page: "",
+          name: reduxState4.name,
+        })
+      );
+      dispatch(
+        isInUse({
+          ...state,
+          brandId: "",
+          brandName: false,
+          name: reduxState4.name,
+        })
+      );
     } else {
-      console.log(reduxState4)
-      setState({ ...state, brandId: e.target.value, brandName: e.target.name, page: '', name: reduxState4.name });
-      dispatch(preFilter({ ...state, brandId: e.target.value, brandName: e.target.name, page: '', name: reduxState4.name }));
-      dispatch(isInUse({ ...state, brandId: e.target.value, brandName: e.target.name, name: reduxState4.name }))
+      console.log(reduxState4);
+      setState({
+        ...state,
+        brandId: e.target.value,
+        brandName: e.target.name,
+        page: "",
+        name: reduxState4.name,
+      });
+      dispatch(
+        preFilter({
+          ...state,
+          brandId: e.target.value,
+          brandName: e.target.name,
+          page: "",
+          name: reduxState4.name,
+        })
+      );
+      dispatch(
+        isInUse({
+          ...state,
+          brandId: e.target.value,
+          brandName: e.target.name,
+          name: reduxState4.name,
+        })
+      );
     }
   };
 
   const handleSort = (e) => {
     setPage(1);
-    setState({ ...state, sort: e.target.value, page: '', name: reduxState4.name, sortName: e.target.value });
-    dispatch(preFilter({ ...state, sort: e.target.value, page: '', name: reduxState4.name, sortName: e.target.value }));
-    dispatch(isInUse({ ...state, typeName: e.target.value, name: reduxState4.name, sortName: e.target.value }));
+    setState({
+      ...state,
+      sort: e.target.value,
+      page: "",
+      name: reduxState4.name,
+      sortName: e.target.value,
+    });
+    dispatch(
+      preFilter({
+        ...state,
+        sort: e.target.value,
+        page: "",
+        name: reduxState4.name,
+        sortName: e.target.value,
+      })
+    );
+    dispatch(
+      isInUse({
+        ...state,
+        typeName: e.target.value,
+        name: reduxState4.name,
+        sortName: e.target.value,
+      })
+    );
   };
 
   const handleType = (e) => {
     setPage(1);
-    setState({ ...state, type: e.target.value, name: reduxState4.name, typeName: e.target.value });
-    dispatch(preFilter({ ...state, type: e.target.value, name: reduxState4.name, typeName: e.target.value }));
-    dispatch(isInUse({ ...state, typeName: e.target.name, name: reduxState4.name, typeName: e.target.value }));
+    if (reduxState4.typeName === e.target.value) {
+      setState({
+        ...state,
+        type: false,
+        page: "",
+        name: reduxState4.name,
+        typeName: false,
+      });
+      dispatch(
+        preFilter({
+          ...state,
+          type: false,
+          page: "",
+          name: reduxState4.name,
+          typeName: false,
+        })
+      );
+      dispatch(
+        isInUse({
+          ...state,
+          type: false,
+          page: "",
+          name: e.target.value,
+          typeName: false,
+        })
+      );
+    } else {
+      setState({
+        ...state,
+        type: e.target.value,
+        page: "",
+        name: reduxState4.name,
+        typeName: e.target.value,
+      });
+      dispatch(
+        preFilter({
+          ...state,
+          type: e.target.value,
+          page: "",
+          name: reduxState4.name,
+          typeName: e.target.value,
+        })
+      );
+      dispatch(
+        isInUse({
+          ...state,
+          typeName: e.target.name,
+          page: "",
+          name: reduxState4.name,
+          typeName: e.target.value,
+        })
+      );
+    }
+  };
+
+  const handleName = (e) => {
+    dispatch(
+      preFilter({
+        ...state,
+        name: "",
+      })
+    );
   };
 
   const handlePage = (e, p) => {
-    setState({ ...state, page: (p), name: reduxState4.name });
-    dispatch(preFilter({ ...state, page: (p), name: reduxState4.name }));
+    setState({ ...state, page: p, name: reduxState4.name });
+    dispatch(preFilter({ ...state, page: p, name: reduxState4.name }));
     setPage(p);
   };
-
-  // console.log(reduxState4);
-  console.log(state)
 
   return (
     <>
@@ -149,37 +297,75 @@ export default function Categorys() {
         </ButtonGroup>
       </Box>
 
-      <Box justifyContent={"center"} alignItems="center" display={"grid"} marginBottom={8}>
+      <Box
+        justifyContent={"center"}
+        alignItems="center"
+        display={"grid"}
+        marginBottom={8}
+      >
         <ButtonGroup
           variant="contained"
           aria-label="outlined primary button group"
         >
-          <Button
-            onClick={handleReset} value={{}} color="success">
+          <Button onClick={handleReset} value={{}} color="success">
             Limpiar Filtros
           </Button>
-          <Button variant={state.type === "salePrice" ? "outlined" : "contained"}
-            onClick={handleType} value={"salePrice"} color="success">
+          <Button
+            variant={state.type === "salePrice" ? "outlined" : "contained"}
+            onClick={handleType}
+            value={"salePrice"}
+            color="success"
+          >
             Precio
           </Button>
-          <Button variant={state.type === (false || "id") ? "outlined" : "contained"}
-            onClick={handleType} value={"id"} color="success">
-            Creacion
+          <Button
+            variant={state.type === "id" ? "outlined" : "contained"}
+            onClick={handleType}
+            value={"id"}
+            color="success"
+          >
+            Mas Reciente
           </Button>
-          <Button variant={state.sort === "ASC" ? "outlined" : "contained"}
-            onClick={handleSort} value={"ASC"} color="success">
+          <Button
+            variant={state.sort === "ASC" ? "outlined" : "contained"}
+            onClick={handleSort}
+            value={"ASC"}
+            color="success"
+          >
             Ascendente
           </Button>
-          <Button variant={reduxState4?.sortName == (false || "DESC") ? "outlined" : "contained"}
-            onClick={handleSort} value={"DESC"} color="success">
+          <Button
+            variant={state.sort === "DESC" ? "outlined" : "contained"}
+            onClick={handleSort}
+            value={"DESC"}
+            color="success"
+          >
             Descendente
           </Button>
         </ButtonGroup>
+        <Box
+          justifyContent={"center"}
+          alignItems="center"
+          display={"grid"}
+          marginBottom={8}
+          value={reduxState4.name}
+          onClick={handleName}
+        >
+          {reduxState4 && reduxState4.name ? (
+            <Button color="secondary">{reduxState4.name}</Button>
+          ) : null}
+        </Box>
       </Box>
       {Array.isArray(reduxState2?.content) && !reduxState2?.content[0] ? (
         <Alert severity="error">No se encontraron Productos!</Alert>
       ) : null}
-      <Grid container gridColumn={3} spacing={4} justifyContent="center" alignItems={"center"}>
+      <Grid
+        container
+        gridColumn={3}
+        spacing={4}
+        justifyContent="center"
+        alignItems={"center"}
+      >
         {reduxState2 &&
           reduxState2.content.map((e) => (
             <Grid item mb={5} sm={3.1} key={e.id}>
@@ -217,4 +403,4 @@ export default function Categorys() {
       </Box>
     </>
   );
-};
+}
