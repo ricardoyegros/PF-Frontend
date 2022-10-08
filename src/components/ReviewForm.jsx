b508f89866fb4ce50562091ce91a294922b4ba56
Miqueas Ledesma <Mickeas98@gmail.com>
2022-10-08 06:01:17 -0300

    fix-reviews

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,6 +1,6 @@
 import { Button, List, ListItem, MenuItem, TextField } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux';
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { getReviews, postReview } from '../redux/actions/reviewsActions';
 
 export const ReviewForm = ({ productId }) => {
@@ -10,9 +10,7 @@ export const ReviewForm = ({ productId }) => {
 
     const [state, setState] = useState({ userId: localStorage.id, productId });
 
-    useEffect(() => {
-        setState({ userId: localStorage.id, productId })
-    }, [dispatch])
+    const reduxState = useSelector(state => state.reviewsReducer.reviews);
 
     const handleSelect = (e) => {
         setState({
@@ -28,47 +26,53 @@ export const ReviewForm = ({ productId }) => {
         });
     };
 
-    const handleSubmit = () => {
-        dispatch(postReview(state));
-        dispatch(getReviews(productId));
-        setState({ userId: localStorage.id, productId });
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(postReview({ ...state }));
+        dispatch(getReviews({ ...productId }));
+        setState({ ...state, userId: localStorage.id, productId, detail: null, stars: null });
+    };
 
-    }
 
-    return (
-        <>
-            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                <ListItem>
+    if (state.detail === null) dispatch(getReviews(productId));
 
-                    <form noValidate autoComplete="off" style={{
-                        justifyContent: "center", alignItems:
-                            "center", display: "grid"
-                    }}>
-                        <TextField
-                            select
-                            label="RATING"
-                            placeholder="Selecciona un Rating..."
-                            variant="outlined"
-                            fullWidth
-                            required
-                            name="rating"
-                            value={state.rating}
-                            onChange={handleSelect}
-                        >
-                            {arr.map((e) => (
-                                <MenuItem key={e} value={e}>
-                                    {e}
-                                </MenuItem>
-                            ))}
-                        </TextField>
-                        <TextField onChange={handleTextInput} id="standard-basic" label="Detalles" multiline rows={4} />
 
-                        <Button onClick={handleSubmit} >Submit</Button>
-                    </form>
-                </ListItem>
-            </List>
-        </>
-    )
+    // if ()
+
+        return (
+            <>
+                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                    <ListItem>
+
+                        <form onSubmit={handleSubmit} noValidate autoComplete="off" style={{
+                            justifyContent: "center", alignItems:
+                                "center", display: "grid"
+                        }}>
+                            <TextField
+                                select
+                                label="RATING"
+                                placeholder="Selecciona un Rating..."
+                                variant="outlined"
+                                fullWidth
+                                required
+                                name="rating"
+                                value={state.rating}
+                                onChange={handleSelect}
+                            >
+                                {arr.map((e) => (
+                                    <MenuItem key={e} value={e}>
+                                        {e}
+                                    </MenuItem>
+                                ))}
+                            </TextField>
+                            <TextField onChange={handleTextInput} id="standard-basic" label="Detalles" multiline rows={4} />
+
+                            <Button type='submit' >Submit</Button>
+                        </form>
+                    </ListItem>
+                </List>
+            </>
+        )
 }
 
 
