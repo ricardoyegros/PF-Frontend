08c57f3925a5d080a4b404d79e5aef5ddb9a6241
Ricardo Yegros <ricardo_yegros@hotmail.com>
2022-10-12 01:54:12 -0300

    Update cleartCart.js

--- src/redux/actions/cleartCart.js
+++ src/redux/actions/cleartCart.js
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function cleartCart (email) {
     return function () {
-        axios.post(`https://techstore123.herokuapp.com/carts?email=${email}`)
+        axios.delete(`https://techstore123.herokuapp.com/carts?email=${email}`)
     }
 }
 
