d71e5775218c1122b0685470dc6f83e08e50933c
Billy Mendieta <oscarald.mc@gmail.com>
2022-10-14 22:01:47 -0400

    fix show geo

--- src/App.js
+++ src/App.js
@@ -70,6 +70,7 @@ export default function App() {
 
 
                     <Route path='/userprofile' element={<UserConfig />} />
+                    <Route path='/geo' element={<Geo />} />
 
                 </Routes>
                 <Footer />
