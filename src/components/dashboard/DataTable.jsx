f88e2b45720232d2b1aa052ce10061998de447e6
juliandavidnunesfranco <juliandavidnunesfranco@hotmail.es>
2022-10-15 23:31:57 -0500

    Se actualiza el catalogo

--- src/components/dashboard/DataTable.jsx
+++ src/components/dashboard/DataTable.jsx
@@ -3,31 +3,49 @@ import { DataGrid } from '@mui/x-data-grid';
 import Sidebar from './Sidebar'
 import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../../redux/actions/adminProductAction';
+import { Category, Summarize } from '@mui/icons-material';
 
 
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'products', headerName: 'Products', width: 130 },
+  { field: 'products', headerName: 'Products', width: 280 },
   { field: 'brand', headerName: 'Brand', width: 130 },
+  { 
+    field:'category',
+    headerName: 'Category',
+    width: 130,
+  },  
   {
     field: 'stock',
     headerName: 'Stock',
-    allowNull: false,
     default: 0,
+    sortable: true,
     type: 'number',
     width: 90,
     
-  },
+  },  
   {
     field: 'available',
     headerName: 'Available',
     description: 'This column has a value getter and is not sortable.',
     sortable: false,
-    width: 160,
+    width: 130,
     valueGetter: (params) =>
       `${params.row.stock > 0 ? 'Available' : 'Not available' }`,
   },
+  {
+    field:'purchasePrice',
+    headerName: 'Purchase Price',
+    type: 'number',
+    width: 90,
+  },
+  {
+    field:'salePrice',
+    headerName: 'Sale Price',
+    type: 'number',
+    width: 90,
+  }
 ];
 
 export default function DataTable() {
@@ -38,12 +56,18 @@ export default function DataTable() {
   useEffect(() => {
     dispatch(getProducts())
   }, [dispatch])
+
   const showProducts = products.map(prod => ({
     id: prod.id,
     brand: prod.brand.name,
     products: prod.name,
-    stock: prod.stock
+    stock: prod.stock,
+    category: prod.category.name,
+    purchasePrice: prod.purchasePrice,
+    salePrice: prod.salePrice,
+
   }))
+  
   rows = [...showProducts]
 
   console.log(rows)
