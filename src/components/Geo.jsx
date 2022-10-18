77982549f90355a935da55d453e940baf95f8ae3
Mauricioam <mauricioam91@gmail.com>
2022-10-18 12:44:20 -0300

    componente Map en home

--- src/components/Geo.jsx
+++ src/components/Geo.jsx
@@ -2,6 +2,7 @@ import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getGeolocation } from "../redux/actions/geoActions"
+import { Box, Typography , Alert} from "@mui/material"
 
 
 function Geo() {
@@ -13,38 +14,60 @@ function Geo() {
 
   const geo = useSelector((state) => state.geoReducer.geo);
   console.log(geo)
-
+  
   const defaultPosition = {
-    lat: -16.495576292607318,
-    lng: -68.1334731733453
+    lat: -34.607510000000005,
+    lng: -58.44661000000001
   };
 
-  const pos = [
-    {
-      lat: -16.495576292607318,
-      lng: -68.1334731733453,
-      title: "titulo 1"
-    },
-    {
-      lat: -16.49856239536191,
-      lng: -68.12415571869423,
-      title: "titulo 2"
-    }
-  ]
 
   const containerStyle = {
-    width: '600px',
-    height: '400px'
-  };
-  const title = "Plazon"
+        width: '600px',
+        height: '400px',  
+}
+
+
+
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyAOI1Z-IoNqr0_-o0XnWapHbivPg0Hhnj4"
   })
   if (!isLoaded) return <div>Loading...</div>;
 
   return (
-    <div>
-      { geo &&
+    <>
+<Box
+display={"flex"}
+justifyContent={"center"}
+alignItems={"center"}
+m={5}
+  >
+<div class="container">
+<div class="shadow-lg p-2 mb-5 bg-white rounded">
+  <Box
+  display={"flex"}
+  justifyContent={"center"} 
+  alignItems={"center"}
+  mt={5}
+  mb={3}
+  ml={3}>
+  <Typography variant='h4' >Encuentra tu sucursal mas cercana</Typography>
+  </Box>
+  <div class="container-fliud" 
+  style={{
+    "width":"100%",
+    "height":"100%",
+    "padding":"2rem"
+     }}>
+    <div class="wrapper row">
+      <div class="col-md-6" 
+      style={{
+      "display": "flex",
+      "justifyContent":"flex-end",
+      "maxWidth":"40rem"
+      
+      }}>
+        
+       { geo &&
         <GoogleMap
         mapContainerStyle={containerStyle}
         center={defaultPosition}
@@ -57,9 +80,89 @@ function Geo() {
           <Marker position={{ lat: p.latitude, lng: p.longitude }} title={p.name} />
         ))}
       </GoogleMap>}
+      
+      </div>
+      <div class=" col-md-6" >
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Nombre</th>
+      <th scope="col">Direccion</th>
+    </tr>
+  </thead>
+  <tbody>
+    {geo.length ? (
+        geo.map((branch) => (
+            <tr >
+            <th scope="row">{branch.name}</th>
+            <td> <a href={`https://www.google.com/maps/@${branch.latitude},${branch.longitude},11z`} target="_blanck"
+               style={{"color":"inherit"}}>
+            {branch.address}</a></td>
+            </tr>
+              
+        ))
+    ) :  <Box display={"flex"} justifyContent={"center"} alignItems={"center"}><Alert severity="error">No se encontraron sucursales!</Alert></Box> }
+  </tbody>
+</table>
 
+        </div>
+      </div>
     </div>
+  </div>
+</div>
+</Box>
+    </>
   )
 }
 
 export default Geo
+
+/* 
+<Box
+display={"flex"}
+justifyContent={"center"}
+alignItems={"center"}
+m={5}
+  >
+<div class="container">
+<div class="shadow-lg p-2 mb-5 bg-white rounded">
+  <div class="container-fliud">
+    <div class="wrapper row">
+      <div class="col-md-6" 
+      style={{
+      "display": "flex",
+      "justifyContent":"center",
+      "alignItems":"center"}}>
+          <div id="pic-1"><img src={detailProduct.images && detailProduct.images[0].url} style={{"maxWidth":"20rem","marginBottom":"0.5rem"}} /></div>
+      </div>
+      <div class="details col-md-6">
+        <h3 class="product-title" style={{"margin": "3rem 1rem 3rem 1rem","textAlign":"justify"}}>{detailProduct.name && detailProduct.name}</h3>
+        <div class="rating">
+          <div class="stars">
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+          </div>
+          <span class="review-no" style={{"margin":"2rem 0 2rem 1rem"}}>41 reviews</span>
+        </div>
+        <p class="product-description" style={{"margin":"2rem 0 2rem 1rem"}}>{detailProduct.description && detailProduct.description}</p>
+        <h4 class="price" style={{"margin":"4rem 0 2rem 1rem"}}>Precio actual <span>${detailProduct.salePrice && detailProduct.salePrice}</span></h4>
+          <div class="action-buttons" 
+          style={{
+           "display":"flex",
+           "gap":"5rem", 
+           "flexDirection": "row",
+           "justifyContent":"center",
+           "margin":"4rem 0 2rem 0"}}>
+        <Button color="success"  onClick={handleClickButton}><ShoppingCartIcon fontSize={"large"}/></Button>
+        <Button color="error"><FavoriteBorderIcon fontSize={"large"}/></Button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div> 
+<Reviews id={i} /> 
+</Box> */
